2a8599f19972ab91ca4e0939b9d287beb3626c4f
Randy <randyisbusy@gmail.com>
2020-12-06 16:53:16 -0700

    updates Node.py

--- Node/Node.py
+++ Node/Node.py
@@ -5,17 +5,37 @@ class Graph:
 
     def show_matrix(self): 
         print(self.matrix) 
+    def set_up_vertex(self, vertex):
+        print(isinstance(vertex, Node))
 
 
 class Node: 
     def __init__(self, data): 
-        self.data = data, 
-    def who_am_i(self): 
-        print(self.data)
+        self.data = data 
+        self.next = None  
+        
+    def get_next_node(self): 
+        return self.next 
+    def set_next_node(self, node):
+            
+            current = self
+             
+            while(current.next is not None): 
+                     
+                     current = current.get_next_node() 
+                     
+            current.next = node 
+                   
+    
 
+building1 = Node("address 101") 
+building2 = Node("address 202")
+building3 = Node("address 303")
+building1.set_next_node(building2) 
+building1.set_next_node(building3) 
+print(building1.next.next.data)
 
-
-# randy = Node("Randy") 
-# candis = Node("Candis") 
-# graph = Graph("graph",  [ [0,1],[1,0]] ) 
-# graph.show_matrix()
+ 
+#print(building1.next.data)
+# graph = Graph("Vertex", [[]]) 
+# graph.set_up_vertex(building1)
