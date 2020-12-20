8688f5e91e87c35d09c55809473fb73429fdbe4b
Randy <randyisbusy@gmail.com>
2020-12-20 08:57:49 -0700

    updates tree.py

--- Node/tree.py
+++ Node/tree.py
@@ -1,6 +1,14 @@
 class BinaryTree:
-    def __init__(self):
-        self.root = None 
+    def __init__(self, data=None):
+        self.parent = None
         self.data = None
-        self.left = None 
-        self.right = None 
+        self.left = None
+        self.right = None
+
+    def set_parent(self, data):
+        self.parent = BinaryTree()
+        self.parent.data = data
+        return f'parent tree set to #{self.parent.data}'
+
+    def add_data(self, data):
+        return self
